8e4fbfca8000243e3847041cad759df8149c9f0e
Megyeri Márk Máté <megyko1234@gmail.com>
2022-11-16 09:15:10 +0100

    Bootsrapt hozzáadas

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmpComponent } from './emp/emp.component';
+import { AboutComponent } from './about/about.component';
+import { GitComponent } from './git/git.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmpComponent,
+    AboutComponent,
+    GitComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
